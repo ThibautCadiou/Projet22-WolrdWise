639322d545af836a607ab875de5e1b5295557d4e
ThibautCadiou <cadioucontact@gmail.com>
2025-04-15 12:46:08 +0200

    ajout du dynamic route with url

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Homepage from './pages/Homepage';
 import PageNotFound from './pages/PageNotFound';
 import Login from './pages/Login';
+import AppLayout from './pages/AppLayout';
+import CityList from './components/CityList';
+import CountryList from './components/CountryList';
+import CityItem from './components/CityItem';
+import City from './components/City';
+
+const BASE_URl = 'http://localhost:9000/';
 
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+
+        const res = await fetch(`${BASE_URl}\cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert('Tehre was an error loading data');
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -13,8 +42,18 @@ export default function App() {
           <Route path='/' element={<Homepage />} />
           <Route path='product' element={<Product />} />
           <Route path='pricing' element={<Pricing />} />
-          <Route path='/login' element={<Login />} />
-          {/* <Route path='app' element={<AppLayout />} /> */}
+          <Route path='login' element={<Login />} />
+          <Route path='app' element={<AppLayout />}>
+            <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route path='cities/:id' element={<City />} />
+            <Route
+              path='countries'
+              element={<CountryList cities={cities} isLoading={isLoading} />}
+            />
+
+            <Route path='form' element={<p>Form</p>} />
+          </Route>
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/components/Map.jsx
@@ -0,0 +1,9 @@
+import AppNav from './AppNav';
+import styles from './Map.module.css';
+import Sidebar from './Sidebar';
+
+function Map() {
+  return <div className={styles.app}>Map</div>;
+}
+
+export default Map;
